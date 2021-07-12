cc06f21f9ab1d3864d6b63072f3a945724b30b0e
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-12 22:01:40 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Feature from "./components/Feature";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
+import { GlobalStyle } from "./globalStyles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <GlobalStyle />
+        <Hero />
+        <Products heading="Choose your favourite" data={productData} />
+        <Feature />
+        <Products heading="Sweat Treats for you" data={productDataTwo} />
+        <Footer />
+      </Router>
+    </>
   );
 }
 
